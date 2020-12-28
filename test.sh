87c7623abd508c2d14a468a2c77612ec99659484
Mathis Hoffmann <m.hoffmann@hoffdoc.de>
2020-12-28 16:12:14 +0100

    init

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -A kurs00042
+#SBATCH -p kurs00042
+#SBATCH --reservation=kurs00042
+#SBATCH -J HelloWorld
+#SBATCH --mail-type=ALL
+#SBATCH -e /home/kurse/kurs00042/nh51mini/SPP/Praktikum1_Gruppe133/Hoffmann/helloWorld-%j.err
+#SBATCH -o /home/kurse/kurs00042/nh51mini/SPP/Praktikum1_Gruppe133/Hoffmann/helloWorld-%j.out
+#SBATCH -n 1      
+#SBATCH -c 1
+#SBATCH --mem-per-cpu=100
+#SBATCH -t 00:10:00
+
+cd /home/kurse/kurs00042/ru44wewy/SPP/Uebungsblatt1_Gruppe133/Aufgabe1/
+./helloWorld
