e03e8bf1a7ff327324d85e39a234580c3fb79f90
Mathis Hoffmann <m.hoffmann@hoffdoc.de>
2020-12-28 16:30:15 +0100

    third commit

--- test.sh
+++ test.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#SBATCH -A kurs00042
-#SBATCH -p kurs00042
-#SBATCH --reservation=kurs00042
-#SBATCH -J HelloWorld
-#SBATCH --mail-type=ALL
-#SBATCH -e /home/kurse/kurs00042/nh51mini/SPP/Praktikum1_Gruppe133/Hoffmann/helloWorld-%j.err
-#SBATCH -o /home/kurse/kurs00042/nh51mini/SPP/Praktikum1_Gruppe133/Hoffmann/helloWorld-%j.out
-#SBATCH -n 1      
-#SBATCH -c 1
-#SBATCH --mem-per-cpu=100
-#SBATCH -t 00:10:00
+#sbatch -a kurs00042
+#sbatch -p kurs00042
+#sbatch --reservation=kurs00042
+#sbatch -j HelloWorld
+#sbatch --mail-type=all
+#sbatch -e /home/kurse/kurs00042/nh51mini/SPP/Praktikum1_Gruppe133/Hoffmann/helloWorld-%j.err
+#sbatch -o /home/kurse/kurs00042/nh51mini/SPP/Praktikum1_Gruppe133/Hoffmann/helloWorld-%j.out
+#sbatch -n 1      
+#sbatch -c 1
+#sbatch --mem-per-cpu=100
+#sbatch -t 00:10:00
 
-cd /home/kurse/kurs00042/ru44wewy/SPP/Uebungsblatt1_Gruppe133/Aufgabe1/
+cd /home/kurse/kurs00042/nh51mini/SPP/Uebungsblatt1_Gruppe133/Aufgabe1/
 ./helloWorld
