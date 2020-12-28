f7696d2223d0baf26d9cc09a05b258200d1540f6
Mathis Hoffmann <62606713+mathish00@users.noreply.github.com>
2020-12-28 17:30:34 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,6 +4,8 @@
 #SBATCH --reservation=kurs00042
 #SBATCH -J HelloWorld
 #SBATCH --mail-type=all
+#SBATCH -e /home/kurse/kurs00042/nh51mini/SPP/Praktikum1_Gruppe133/Hoffmann/helloWorld.err
+#SBATCH -o /home/kurse/kurs00042/nh51mini/SPP/Praktikum1_Gruppe133/Hoffmann/helloWorld.out
 #SBATCH -n 1      
 #SBATCH -c 1
 #SBATCH --mem-per-cpu=100
