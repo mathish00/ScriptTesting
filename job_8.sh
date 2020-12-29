fbb889d6d1508f4a5513bf6ba2a869319917879e
Mathis Hoffmann <62606713+mathish00@users.noreply.github.com>
2020-12-29 14:26:46 +0100

    Update job_8.sh

--- job_8.sh
+++ job_8.sh
@@ -4,16 +4,16 @@
 #SBATCH --reservation=kurs00042
 #SBATCH -J Zeitmessung
 #SBATCH --mail-type=all
-#SBATCH -e /home/kurse/kurs00042/nh51mini/ScriptTesting/3c_8_Threads.err.%j
-#SBATCH -o /home/kurse/kurs00042/nh51mini/ScriptTesting/3c_8_Threads.out.%j
+#SBATCH -e /home/kurse/kurs00042/nh51mini/ScriptTesting/4b_1_Thread.err.%j
+#SBATCH -o /home/kurse/kurs00042/nh51mini/ScriptTesting/4b_1_Thread.out.%j
 #SBATCH -n 1      
-#SBATCH -c 8
+#SBATCH -c 1
 #SBATCH --mem-per-cpu=100
 #SBATCH -t 00:10:00
 
 ml load gcc
 
 cd /home/kurse/kurs00042/nh51mini/SPP/Praktikum1_Gruppe133/Hoffmann/
-gcc -fopenmp dotproduct.c -lm
-export OMP_NUM_THREADS=8
+g++ -fopenmp heated-plate-parallel.cpp
+export OMP_NUM_THREADS=1
 ./a.out
