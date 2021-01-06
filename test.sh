84f89ea7f56f8d79f1dda5abbd73719daf8944ef
Mathis Hoffmann <62606713+mathish00@users.noreply.github.com>
2021-01-06 11:48:54 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -7,12 +7,11 @@
 #SBATCH -e /home/kurse/kurs00042/nh51mini/ScriptTesting/test.err.%j
 #SBATCH -o /home/kurse/kurs00042/nh51mini/ScriptTesting/test.out.%j
 #SBATCH -n 1      
-#SBATCH -c 1
+#SBATCH -c 4
 #SBATCH --mem-per-cpu=100
 #SBATCH -t 00:10:00
 
-ml load gcc
-cd /home/kurse/kurs00042/nh51mini/SPP/Uebungsblatt1_Gruppe133/Aufgabe1/
-gcc helloWorld.c
-./a.out
+cd /home/kurse/kurs00042/nh51mini/SPP/Praktikum1_Gruppe133/Hoffmann/
+gcc -o hello -fopenmp hello-openmp.c
+./hello
 
