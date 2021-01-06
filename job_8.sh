b65b9de1e8e317d14f61c35e0f0fc7b7ad2a31a5
Mathis Hoffmann <62606713+mathish00@users.noreply.github.com>
2021-01-06 15:14:39 +0100

    Update job_8.sh

--- job_8.sh
+++ job_8.sh
@@ -8,7 +8,7 @@
 #SBATCH -o /home/kurse/kurs00042/nh51mini/ScriptTesting/4b_8_Threads.out.%j
 #SBATCH -n 1      
 #SBATCH -c 8
-#SBATCH --mem-per-cpu=100
+#SBATCH --mem-per-cpu=1750
 #SBATCH -t 00:10:00
 
 cd /home/kurse/kurs00042/nh51mini/SPP/Praktikum1_Gruppe133/Hoffmann/
